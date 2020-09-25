9917f959c91d523dbfefad4f4bd947de8de2a3cd
Daniel-Sean Folkes <65097622+DSeanHD@users.noreply.github.com>
2020-09-25 14:35:02 -0400

    Update Python_Quiz.py
    I made the code more readable by leaving a space between operators and I fixed the percentage part at the bottom to allow the percentage sign to be with the percentage value without leaving a space.

--- Python_Quiz.py
+++ Python_Quiz.py
@@ -5,17 +5,17 @@ time.sleep(2)
 print("The quiz where we tell you countries/states/provinces and you tell thier capital")
 time.sleep(3)
 name=input("What is your name? ")
-print("Nice to meet you", name, "!")
+print("Nice to meet you", name + "!")
 time.sleep(1)
 age=int(input("How old are you? "))
 
-if age<=11:
+if age <= 11:
   print("Ok so you're a kid")
 elif age in range(12, 17):
   print("Ok so you're a teenager")
 elif age in range(18, 59):
   print("Ok so you're an adult")
-elif age>=60:
+elif age >= 60:
   print("Ok so you're an elder")
 else:
   int(input("Please enter your age as a number "))
@@ -33,106 +33,109 @@ print("You're also not allowed to use google and if you get a question wrong, we
 #Scores
 score = 0
 print("Score:", score)
-usa=input("What is the capital of the USA? ").lower()
+usa = input("What is the capital of the USA? ").lower()
 
 #Quiz
-if usa=="washington dc":
+if usa == "washington dc":
   print("Correct")
   score += 1
   print("Score", score)
 else:
   print("Incorrect, it's Washington D.C.")
-  score-=1
+  score -= 1
   print("Score", score)
 
 time.sleep(1)
-jamaica=input("What is the capital of Jamaica? ").lower()
+jamaica = input("What is the capital of Jamaica? ").lower()
 
-if jamaica=="kingston":
+if jamaica == "kingston":
   print("Correct")
-  score+=1
+  score += 1
   print("Score:", score)
 else:
   print("Incorrect, it's Kingston")
-  score-=1
+  score -= 1
   print("Score:", score)
 
 time.sleep(1)
-brazil=input("What is the capital of Brazil? ").lower()
+brazil = input("What is the capital of Brazil? ").lower()
 
-if brazil=="brasilia":
+if brazil == "brasilia":
   print("Correct")
-  score+=1
+  score += 1
   print("Score:", score)
 else:
   print("Incorrect, it's Brasilia")
-  score-=1
+  score -= 1
   print("Score:", score)
 
 time.sleep(1)
-new_york=input("What is the capital of New York? ").lower()
+new_york = input("What is the capital of New York? ").lower()
 
-if new_york=="albany":
+if new_york == "albany":
   print("Correct")
   score+=1
   print("Score:", score)
 else:
   print("Incorrect, it's Albany")
-  score-=1
+  score -= 1
   print("Score:", score)
 
 time.sleep(1)
-california=input("What is the capital of california? ").lower()
+california = input("What is the capital of california? ").lower()
 
-if california=="sacremento":
+if california == "sacremento":
   print("Correct")
-  score+=1
+  score += 1
   print("Score:", score)
 else:
   print("Incorrect, it's Sacremento")
-  score-=1
+  score -= 1
   print("Score:", score)
 
 time.sleep(1)
-nigeria=input("What is the capital of Nigeria? ").lower()
+nigeria = input("What is the capital of Nigeria? ").lower()
 
-if nigeria=="abuja":
+if nigeria == "abuja":
   print("Correct")
-  score+=1
+  score += 1
   print("Score:", score)
 else:
   print("Incorrect, it's Abuja")
-  score-=1
+  score -= 1
   print("Score:", score)
 
 time.sleep(1)
-indonesia=input("What is the capital of Indonesia? ").lower()
+indonesia = input("What is the capital of Indonesia? ").lower()
 
-if indonesia=="jakarta":
+if indonesia == "jakarta":
   print("Correct")
-  score+=1
+  score += 1
   print("Score", score)
 else:
   print("Incorrect, it's Jakarta")
-  score-=1
+  score -= 1
   print("Score:", score)
 
 time.sleep(1)
-poland=input("What is the capital of Poland? ").lower()
+poland = input("What is the capital of Poland? ").lower()
 
-if poland=="warsaw":
+if poland == "warsaw":
   print("Correct")
-  score+=1
+  score += 1
   print("Score:", score)
 else:
   print("Incorrect, it's Warsaw")
-  score-=1
+  score -= 1
   print("Score:", score)
 
 time.sleep(2)
 print("You got", score ,"out of 8 in this quiz")
 time.sleep(1)
-print("Which means you got", (score / 8) * 100 ,"%")
+
+percent = (score / 8) * 100
+
+print("Which means you got " + str(percent) + "%")
 time.sleep(1)
 print("I hope you guys enjoyed it :)")
 print("Peace!")
